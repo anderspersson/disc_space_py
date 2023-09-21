7e589670218696ed5224930bcad3ef988ed163ca
Anders Persson <anders.u.persson@gmail.com>
2023-09-21 09:32:54 +0200

    Added more calc and also size

--- check_space.py
+++ check_space.py
@@ -2,15 +2,26 @@
 
 # importing shutil module
 import shutil
+import math
+import sys
 
 # Path
 path = "/"
 
-# Get the disk usage statistics
-# about the given path
+args = sys.argv[1:]
+
+SizeType = "Bytes" # Default type
+if( len(args) < 0):
+    SizeType = args[0]
+
 stat = shutil.disk_usage(path)
+used_p = math.floor((stat.used / stat.free) * 100)
+left_p = 100 - used_p
 
 # Print disk usage statistics
 print("Disk usage statistics:")
-print(stat)
+print(f"Free: {stat.free} {SizeType}")
+print(f"Used: {stat.used} {SizeType}")
+print(f"Used: {used_p} %")
+print(f"Left: {left_p} %: ")
 
