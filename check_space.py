c48b1206c14da67c46407e6ca552daf6890bb61c
Anders Persson <anders.persson@adbriq.com>
2023-09-22 13:18:42 +0200

    Added Type Calcs

--- check_space.py
+++ check_space.py
@@ -5,36 +5,50 @@ import shutil
 import math
 import sys
 
+def TranslateTypeSize(stat,sizeType):
 
-path = "/"
+    match sizeType.upper():
+        case "KB":
+            adj = 1024
+        case "MB":
+            adj = 1048576
+        case "GB":
+            adj = 1073741824
+        case "TB":
+            adj = 1099511627776
+    stat.total = stat.total / adj
+    stat.free = stat.free / adj
+    stat.used = stat.used / adj
+    return stat
 
-if( len(sys.argv) < 2):
-        print("check_space.py SizeType Path")
-        print("Example:\ncheck_space.py MB /home")
-        exit()
+def main():    
+    if( len(sys.argv) < 2):
+            print("check_space.py SizeType Path")
+            print("Example:\ncheck_space.py MB /home")
+            exit()
 
-args = sys.argv[1:]
+    args = sys.argv[1:]
+
+    SizeType = "Bytes" # Default type
+    if( len(args) > 1):
+        SizeType = args[0]
+        path = args[1]
+
+    stat = shutil.disk_usage(path)
+    # stat = TranslateTypeSize(stat,SizeType)
+    used_p = math.floor((stat.used /stat.total ) * 100)
+    left_p = 100 - used_p
+
+    # Print disk usage statistics
+    print("Disk usage statistics:")
+    print(f"Total: {stat.total} {SizeType}")
+    print(f"Free: {stat.free} {SizeType}")
+    print(f"Used: {stat.used} {SizeType}")
+    print(f"Used: {used_p} %")
+    print(f"Left: {left_p} %: ")
+    print(stat)
 
 
-SizeType = "Bytes" # Default type
-if( len(args) > 1):
-    SizeType = args[0]
-    path = args[1]
 
-stat = shutil.disk_usage(path)
-used_p = math.floor((stat.used / stat.free) * 100)
-left_p = 100 - used_p
+main()
 
-# Print disk usage statistics
-print("Disk usage statistics:")
-print(f"Free: {stat.free} {SizeType}")
-print(f"Used: {stat.used} {SizeType}")
-print(f"Used: {used_p} %")
-print(f"Left: {left_p} %: ")
-
-
-#1 KB = 1,024 Bytes
-#1 MB = 1024KB = 1,048,576 Bytes
-#1 GB = 1024MB = 1,048,576 KB = 1,073,741,824 Bytes
-#1 TB = 1024 GB = 1,048,576 
-#MB = 8,388,608 KB = 1,099,511,627,776 Bytes
