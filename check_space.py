37d65303416c7a39e25a4b0c09a8dc4809dfa2e1
Anders Persson <anders.u.persson@gmail.com>
2023-10-06 23:09:51 +0200

    Clean up and formating output

--- check_space.py
+++ check_space.py
@@ -1,49 +1,52 @@
-# Python program to explain shutil.disk_usage() method
+# check_space 
+# Check diskspace in diffrent size types
+# 2023 Anders Persson
+# MIT Licence
 
 import shutil
 import math
 import sys
 
-def TranslateTypeSize(stat,sizeType):
-
-    match sizeType.upper():
+def translate_type_size(stat,size_type):
+    match size_type.upper():
         case "KB":
-            adj = 1024
+            SIZE_CONST = 1024
         case "MB":
-            adj = 1048576
+            SIZE_CONST = 1048576
         case "GB":
-            adj = 1073741824
+            SIZE_CONST = 1073741824
         case "TB":
-            adj = 1099511627776
-    return {"total":round(stat.total / adj,2),"free": round(stat.free / adj,2), "used":round(stat.used / adj,2)}
+            SIZE_CONST = 1099511627776
+    return {"total":round(stat.total / SIZE_CONST,2),"free": round(stat.free / SIZE_CONST,2), "used":round(stat.used / SIZE_CONST,2)}
 
 def main():    
     if( len(sys.argv) < 2):
-            print("check_space.py SizeType Path")
+            print("Help!")
+            print("check_space.py <KB|MB|GB|TB> Path")
             print("Example:\ncheck_space.py MB /home")
             exit()
 
     args = sys.argv[1:]
 
-    SizeType = "Bytes" # Default type
+    size_type = "KB" # Default type
     if( len(args) > 1):
-        SizeType = args[0]
+        size_type = args[0]
         path = args[1]
 
     stat = shutil.disk_usage(path)
-    diskStat = TranslateTypeSize(stat,SizeType)
-    print( diskStat )
-    used_p = math.floor((diskStat["used"] / diskStat["total"] ) * 100)
+    disk_stat = translate_type_size(stat,size_type)
+    print( disk_stat )
+    used_p = math.floor((disk_stat["used"] / disk_stat["total"] ) * 100)
     left_p = 100 - used_p
 
     # Print disk usage statistics
     print("Disk usage statistics:")
-    print(f"Total: {diskStat['total']} {SizeType}")
-    print(f"Free: {diskStat['free']} {SizeType}")
-    print(f"Used: {diskStat['used']} {SizeType}")
-    print(f"Used: {used_p} %")
-    print(f"Left: {left_p} %: ")
+    print(f"Free:  {disk_stat['free']:>10} {size_type}")
+    print(f"Used:  {disk_stat['used']:>10} {size_type}")
+    print(f"Total: {disk_stat['total']:>10} {size_type}")
+    print(f"Used:  {used_p:>10} %")
+    print(f"Left:  {left_p:>10} %")
 
-
-main()
+if __name__ == "__main__":
+    main()
 
