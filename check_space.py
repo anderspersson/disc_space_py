2bbe5761673c15b27cb74502eb437c82b703ead7
Anders Persson <anders.u.persson@gmail.com>
2023-10-07 22:59:46 +0200

    Add parameter handler

--- check_space.py
+++ check_space.py
@@ -6,9 +6,18 @@
 import shutil
 import math
 import sys
+import argparse
+
+parser = argparse.ArgumentParser(description="check_space",
+                                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-t", "--type", choices=['KB', 'MB', 'GB', 'TB'], help="Size Type")
+parser.add_argument("-s", "--silent", action="store_true", help="Only numbers in result")
+parser.add_argument("path", help="Drive and/or path")
+args = parser.parse_args()
+config = vars(args)
 
 def translate_type_size(stat,size_type):
-    match size_type.upper():
+    match str(size_type).upper():
         case "KB":
             SIZE_CONST = 1024
         case "MB":
@@ -17,33 +26,44 @@ def translate_type_size(stat,size_type):
             SIZE_CONST = 1073741824
         case "TB":
             SIZE_CONST = 1099511627776
+        case _:
+            SIZE_CONST = 1
+
     return {"total":round(stat.total / SIZE_CONST,2),"free": round(stat.free / SIZE_CONST,2), "used":round(stat.used / SIZE_CONST,2)}
 
 def main():    
-    if( len(sys.argv) < 2):
-            print("Help!")
-            print("check_space.py <KB|MB|GB|TB> Path")
-            print("Example:\ncheck_space.py MB /home")
-            exit()
+    if config["help"] == True:
+        help = """
+        check_space for checking diskspace
+        Argument
+        -t, --type type to convert result to KB, MB, TB.
+        -s, --silent Only number in result.
+        path Drive and/or path.
 
-    args = sys.argv[1:]
+        Example:
+        python .\check_space.py c:\
+        python .\check_space.py -t GB c:\
+        python .\check_space.py -t GB -v c:\
+        """
+        print(help)
+        exit()
 
-    size_type = "KB" # Default type
-    if( len(args) > 1):
-        size_type = args[0]
-        path = args[1]
-
-    stat = shutil.disk_usage(path)
-    disk_stat = translate_type_size(stat,size_type)
-    print( disk_stat )
+    stat = shutil.disk_usage(config["path"])
+    disk_stat = translate_type_size(stat,config["type"])
     used_p = math.floor((disk_stat["used"] / disk_stat["total"] ) * 100)
     left_p = 100 - used_p
 
+    if config["silent"] == true:
+        print(disk_stat['free'])
+        exit()
+
     # Print disk usage statistics
     print("Disk usage statistics:")
-    print(f"Free:  {disk_stat['free']:>10} {size_type}")
-    print(f"Used:  {disk_stat['used']:>10} {size_type}")
-    print(f"Total: {disk_stat['total']:>10} {size_type}")
+    if config["type"] == None:
+        config["type"] = "Byte"
+    print(f"Free:  {disk_stat['free']:>10} {config['type']}")
+    print(f"Used:  {disk_stat['used']:>10} {config['type']}")
+    print(f"Total: {disk_stat['total']:>10} {config['type']}")
     print(f"Used:  {used_p:>10} %")
     print(f"Left:  {left_p:>10} %")
 
